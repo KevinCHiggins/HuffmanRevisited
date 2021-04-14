a3cca56861106a1eaff158eb66ba24e3adfbacb9
Kevin Higgins <kevincolmhiggins:gmail.com>
2021-04-14 18:05:51 +0100

    Removed fixed path reference in tests

--- src/test/java/huffmanrevisited/TestData.java
+++ src/test/java/huffmanrevisited/TestData.java
@@ -17,11 +17,13 @@ public class TestData {
     static byte[] ZERO_BYTE = new byte[]{0};
     static byte[] UP_TO_EIGHT_INSTANCES_OF_SINGLE_CHAR_ENCODED_DATA = ZERO_BYTE;
     static byte[] ONE_CHAR_COMPACTED_TREE = new byte[]{(byte)128, (byte)16, (byte)128}; 
-    static String IN_PATH = "C:\\Users\\Kevin\\Documents\\NetBeansProjects\\HuffmanRevisited\\src\\main\\java\\huffmanrevisited\\input.txt";
-    static String COMP_PATH = "C:\\Users\\Kevin\\Documents\\NetBeansProjects\\HuffmanRevisited\\src\\main\\java\\huffmanrevisited\\compressed.huf";
-    static String ALT_PATH = "C:\\Users\\Kevin\\Documents\\NetBeansProjects\\HuffmanRevisited\\src\\main\\java\\huffmanrevisited\\compressed2.huf";
+    
+    static String BASE_PATH = ""; 
+    static String IN_PATH =  BASE_PATH + "input.txt";
+    static String COMP_PATH = BASE_PATH + "compressed.huf";
+    static String ALT_PATH = BASE_PATH + "compressed2.huf";
 
-    static String DEC_PATH = "C:\\Users\\Kevin\\Documents\\NetBeansProjects\\HuffmanRevisited\\src\\main\\java\\huffmanrevisited\\output.txt";
+    static String DEC_PATH = BASE_PATH + "output.txt";
 
     static File IN_FILE = new File(IN_PATH);
     static File COMP_FILE = new File(COMP_PATH);
